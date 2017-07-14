928e06cf3875ec5eb26a4e95b505d021513a22b6
EhteshamIzhar <ehte6848@gmail.com>
2017-07-15 01:53:34 +0530

    Initial commit Project ChatApp

---
+++ app/src/main/java/com/example/ehte6848/chatapp/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.example.ehte6848.chatapp;
+
+/**
+ * Created by ehte6848 on 15-07-2017.
+ */
+
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left , String message) {
+        // TODO Auto-generated constructor stub
+        super();
+        this.left=left;
+        this.message = message;
+    }
+}
